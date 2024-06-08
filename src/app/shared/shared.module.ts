5248f431ec7f9ce96fc94931268e89da86e4ecfe
Mohamed Sabry <mohamed.sabry@lyveglobal.com>
2024-06-08 15:52:33 +0300

    adding prime components shared

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,10 +1,38 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ButtonModule } from 'primeng/button';
+import { InputTextModule } from 'primeng/inputtext';
+import { BadgeModule } from 'primeng/badge';
+import { MenuModule } from 'primeng/menu';
+import { SelectButtonModule } from 'primeng/selectbutton';
+import { AvatarModule } from 'primeng/avatar';
+import { AvatarGroupModule } from 'primeng/avatargroup'
+import { ToastModule } from 'primeng/toast';
+import { CarouselModule } from 'primeng/carousel';
+import { ChipModule } from 'primeng/chip';
+import { DividerModule } from 'primeng/divider';
+import { CardModule } from 'primeng/card';
+import { ChartModule } from 'primeng/chart';
+import { TabViewModule } from 'primeng/tabview';
+import { ProgressBarModule } from 'primeng/progressbar';
 
 
 const components = [
-  ButtonModule
+  ButtonModule,
+  InputTextModule,
+  BadgeModule,
+  MenuModule,
+  SelectButtonModule,
+  AvatarModule,
+  AvatarGroupModule,
+  ToastModule,
+  CarouselModule,
+  ChipModule,
+  DividerModule,
+  CardModule,
+  ChartModule,
+  TabViewModule,
+  ProgressBarModule
 ]
 
 
