bcf976e93858ebb586b522b146d8bb9aac46ce05
Mohamed Sabry <mohamed.sabry@lyveglobal.com>
2024-06-11 20:54:08 +0300

    updates

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          'sass-loader',
+        ],
+      },
+      {
+        test: /\.css$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new MiniCssExtractPlugin(),
+  ],
+};
